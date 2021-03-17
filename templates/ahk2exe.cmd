d60969908b3d825f38eee5d2fd3510573389ab56
nnebeel <nnebeel@hotmail.com>
2021-03-17 17:12:44 -0600

    Change filename and absolute cmd paths

--- templates/ahk2exe.cmd
+++ templates/ahk2exe.cmd
@@ -1,2 +1,4 @@
-start "%programfiles%\AutoHotkey\Compiler\Ahk2Exe.exe" /in "OfflineExpert for [Part 1].ahk" /out "OfflineExpert for [Part 2].exe" /icon "..\images\lk_square_icon.ico"
-pause
+"%programfiles%\AutoHotkey\Compiler\Ahk2Exe.exe" /in "%cd%\[Part 1].ahk" /out "%cd%\[Part 2].exe" /icon "%cd%\images\lk_square_icon.ico"
+@REM I swear that I had to modify the bin for these courses 
+@REM so they were compatible with older systems or something.
+@REM If necessary, refer to https://www.autohotkey.com/docs/Scripts.htm#ahk2exe
