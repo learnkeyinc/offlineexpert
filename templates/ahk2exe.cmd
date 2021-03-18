3a7588770a4e918c4d7b21d71f1590d8d17a34d9
nnebeel <nnebeel@hotmail.com>
2021-03-18 15:00:17 -0600

    Correct ahk filename in cmd

--- templates/ahk2exe.cmd
+++ templates/ahk2exe.cmd
@@ -1,4 +1,4 @@
-"%programfiles%\AutoHotkey\Compiler\Ahk2Exe.exe" /in "%cd%\[Part 1].ahk" /out "%cd%\[Part 2].exe" /icon "%cd%\images\lk_square_icon.ico"
+"%programfiles%\AutoHotkey\Compiler\Ahk2Exe.exe" /in "%cd%\OfflineExpert for [Part 1].ahk" /out "%cd%\[Part 2].exe" /icon "%cd%\images\lk_square_icon.ico"
 @REM I swear that I had to modify the bin for these courses 
 @REM so they were compatible with older systems or something.
 @REM If necessary, refer to https://www.autohotkey.com/docs/Scripts.htm#ahk2exe
